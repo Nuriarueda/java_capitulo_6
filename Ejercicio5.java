47614f28b56c0ae0bc547ae7ffab590b81fb9c09
Nuria Rueda <ruedareinanuria@gmail.com>
2022-11-12 14:13:41 +0100

    added

---
+++ Ejercicio5.java
@@ -0,0 +1,20 @@
+/*Ejercicio 5 Muestra 50 números enteros aleatorios 
+entre 100 y 199 (ambos incluidos) separados por espacios. 
+Muestra también el máximo, el mínimo y la media de esos números. */
+
+public class Ejercicio5 {
+    public static void main (String[]args) {
+        int maximo = 0;
+        int minimo = 0;
+        int media = 0;
+        int suma = 0;
+
+        System.out.println("\n50 numeros aleatorios entre 100 y 199: ");
+        for (int i=0; i<50; i++) {
+            minimo = n;
+        }
+        if (n > maximo) {
+maximo = n;
+}
+    }
+}
