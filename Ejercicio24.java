6911b0e72809ca0ecbe382aaddae4ccdfe2f02b2
Nuria Rueda <ruedareinanuria@gmail.com>
2022-11-20 13:08:48 +0100

    added

---
+++ Ejercicio24.java
@@ -0,0 +1,18 @@
+/*Ejercicio 24 Escribe un programa que, dado un número introducido por teclado, 
+elija al azar uno de sus dígitos.*/
+
+public class Ejercicio24 {
+    public static void main(String[] args) {
+        System.out.print("Por favor, introduzca un número entero positivo: ");
+        long numeroIntroducido = Long.parseLong(System.console().readLine());
+        long numero = numeroIntroducido;
+        int longitud = 0;
+        do {
+            numero /= 10;
+            longitud++;
+        } while (numero > 0);
+        int posicion = (int)(Math.random() * longitud) + 1;
+        System.out.println((numeroIntroducido / (long)(Math.pow(10, longitud - posicion))) % 10);
+    }
+}
+    
